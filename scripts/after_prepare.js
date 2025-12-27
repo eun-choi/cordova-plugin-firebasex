e43106d8bee52c3c39d5e3cdfd01959e5c7aabb4
eun-choi <54615424+eun-choi@users.noreply.github.com>
2025-12-27 22:55:47 +0900

    Update after_prepare.js

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -27,15 +27,15 @@ var setupEnv = function(){
     PLATFORM = {
         IOS: {
             platformDir: IOS_DIR,
-            dest: IOS_DIR + '/' + appName + '/Resources/GoogleService-Info.plist',
+            dest: IOS_DIR + '/App/Resources/GoogleService-Info.plist',
             src: [
                 'GoogleService-Info.plist',
                 IOS_DIR + '/www/GoogleService-Info.plist',
                 'www/GoogleService-Info.plist'
             ],
-            appPlist: IOS_DIR + '/' + appName + '/' + appName + '-Info.plist',
-            entitlementsDebugPlist: IOS_DIR + '/' + appName + '/Entitlements-Debug.plist',
-            entitlementsReleasePlist: IOS_DIR + '/' + appName + '/Entitlements-Release.plist',
+            appPlist: IOS_DIR + '/App/App-Info.plist',
+            entitlementsDebugPlist: IOS_DIR + '/App/Entitlements-Debug.plist',
+            entitlementsReleasePlist: IOS_DIR + '/App/Entitlements-Release.plist',
             podFile: IOS_DIR + '/Podfile'
         },
         ANDROID: {
